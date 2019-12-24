61a4eabcba0fc210141a89e764b28bad14b2c333
leafguo <812466288@qq.com>
2019-12-24 17:26:57 +0800

    ===添加Leaf_Flash

---
+++ Leaf_flash/leaf_flash.h
@@ -0,0 +1,57 @@
+#ifndef _LEAF_FLASH_H_
+#define _LEAF_FLASH_H_
+
+
+//#define LEAF_FLASH_TEST
+
+
+//#define STM32F103_RB
+//#define STM32F103_RC
+//#define STM32F103_ZE
+
+
+//#ifdef STM32F103_RB
+#define PAGE_SIZE	FLASH_PAGE_SIZE			// 页大小
+#define PAGE120 0x0801E000
+#define PAGE121 0x0801E400
+#define PAGE122 0x0801E800
+#define PAGE123 0x0801EC00
+#define PAGE124 0x0801F000
+#define PAGE125 0x0801F400
+#define PAGE126 0x0801F800
+#define PAGE127 0x0801FC00
+//#endif
+
+
+
+/* 定义一个结构体 */
+typedef struct
+{
+	unsigned int head_addr;	//变量存储的起始页
+	unsigned int page_num;	//变量存储页大小
+}L_node;
+
+
+
+/* 读取函数 */
+unsigned int Find_write_addr(L_node node, unsigned int *addr);
+
+
+
+/* 写入函数 */
+int Write_LeafFlash(L_node node, unsigned char *src, unsigned int size);
+
+
+/* 测试函数 */
+#ifdef LEAF_FLASH_TEST
+void test(void);
+#endif
+
+
+
+
+
+
+
+
+#endif
